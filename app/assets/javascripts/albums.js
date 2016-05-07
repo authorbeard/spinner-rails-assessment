409491d1c72e5e312c30040994613c3b9033108e
authorbeard <msnolan@gmail.com>
2016-05-07 00:48:58 -0400

    began tweaking navbar. Need asset pipeline cleanup

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -13,17 +13,5 @@
 
 
 
-$(document).ready(function() {
-  $(".spin-it").click(function(event){
 
-    var id = parseInt($(this).attr("data"))
-    $.post("/albums/" + id + "/spin", function(data){
-      var albData=JSON.parse(data)
-      $("#album-" + albData["album"]).css("color", "blue").text(albData["spins"] + " Spins")
-      $(".user-spins").text(albData["spins"])
-      $(".last-user-spin").text(Date())
-    })
-    event.preventDefault()
-  })
-})
 
